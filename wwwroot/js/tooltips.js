16219229be190d62dd47f1ce46009f6b86f8f663
bartelRW <bartel@rasche-wessler.de>
2025-11-25 14:49:22 +0100

    :sparkles: Add Bootstrap tooltip support

---
+++ wwwroot/js/tooltips.js
@@ -0,0 +1,5 @@
+﻿// Enable Bootstrap tooltips
+document.addEventListener("DOMContentLoaded", function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
+});
