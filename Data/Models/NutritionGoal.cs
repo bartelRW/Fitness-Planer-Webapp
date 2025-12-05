1b12f679b8e40ebfcb14d1457bed60b9eee55a5c
bartelRW <bartel@rasche-wessler.de>
2025-12-05 12:52:29 +0100

    :sparkles: Add function to add food entries per day

--- Data/Models/NutritionGoal.cs
+++ Data/Models/NutritionGoal.cs
@@ -3,9 +3,9 @@
     public class NutritionGoal
     {
         public int Id { get; set; }
-        public double Calories { get; set; } = 0;
-        public double Protein { get; set; } = 0;
-        public double Carbohydrates { get; set; } = 0;
-        public double Fats { get; set; } = 0;
+        public double Calories { get; set; } = 2400;
+        public double Protein { get; set; } = 90;
+        public double Carbohydrates { get; set; } = 300;
+        public double Fats { get; set; } = 93;
     }
 }
