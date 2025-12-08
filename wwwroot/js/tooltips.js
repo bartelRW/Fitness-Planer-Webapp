cdf401d783347de1cf73a6269ebe627883a7ff58
bartelRW <bartel@rasche-wessler.de>
2025-12-08 12:01:23 +0100

    :fire: Remove unused scripts

--- wwwroot/js/tooltips.js
+++
@@ -1,5 +0,0 @@
-﻿// Enable Bootstrap tooltips
-document.addEventListener("DOMContentLoaded", function () {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-});
