6ba5f43d88d77f370410626f3037f56921a733cd
bartelRW <bartel@rasche-wessler.de>
2025-12-09 11:44:01 +0100

    💹  Create demo chart

---
+++ wwwroot/js/chartHelper.js
@@ -0,0 +1,5 @@
+﻿// Initializes a Chart.js chart on a given canvas element using provided configuration.
+window.renderChart = (canvasId, chartConfig) => {
+    const ctx = document.getElementById(canvasId).getContext('2d');
+    new Chart(ctx, chartConfig);
+};
