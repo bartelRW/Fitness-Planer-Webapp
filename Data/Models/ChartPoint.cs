3ff5b668e894bd7d44944712d33298f7cbc96fcd
bartelRW <bartel@rasche-wessler.de>
2025-12-10 13:56:14 +0100

    :sparkles: Add charts for nutritions with live data

---
+++ Data/Models/ChartPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessPlaner.Data.Models
+{
+    public class ChartPoint
+    {
+        public DateOnly Date { get; set; }
+        public double Value { get; set; }
+        public double NutritionGoal { get; set; }
+    }
+}
