7a58f600e90e5e61560ee72a0ba97e349d023057
bartelRW <bartel@rasche-wessler.de>
2025-12-12 13:26:36 +0100

    :lipstick: Update darkmode toggle

---
+++ Data/Models/BrandTheme.cs
@@ -0,0 +1,10 @@
+﻿namespace FitnessPlaner.Data.Models
+{
+    public class BrandTheme
+    {
+        public string TopRow { get; set; } = string.Empty;
+        public string SideBar { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string BrandBorder { get; set; } = string.Empty;
+    }
+}
