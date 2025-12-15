10f57543f5390762eed61a521b6a9f8ab94dc051
bartelRW <bartel@rasche-wessler.de>
2025-12-15 15:21:05 +0100

    :lipstick: Update UI

--- Data/Models/NutritionStatus.cs
+++ Data/Models/NutritionStatus.cs
@@ -3,6 +3,7 @@
     public class NutritionStatus
     {
         public string Color { get; set; } = string.Empty;
+        public BlazorBootstrap.TooltipColor TooltipColor { get; set; } = default!;
         public string Icon { get; set; } = string.Empty;
         public double Percentage { get; set; }
         public string Message { get; set; } = string.Empty;
